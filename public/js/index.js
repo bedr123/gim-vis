d3b36aada08021887312fea0ce516fe64c865ad1
Muhamed-M <muhamedmuhamedagic03@gmail.com>
2022-04-25 14:42:56 +0200

    Muhamedov push

--- public/js/index.js
+++ public/js/index.js
@@ -24,20 +24,12 @@ dropdownBtn.forEach((btn) => {
 // STICKY HEADER
 const nav = document.querySelector(".navbar");
 const body = document.querySelector("body");
-const navHeight = nav.offsetHeight + "px";
 
 function stickyNav() {
     if (window.scrollY >= 80) {
-        body.style.paddingTop = navHeight;
-        nav.classList.add("sticky");
-    } else if (window.scrollY <= 1) {
-        body.style.paddingTop = 0;
-        nav.classList.remove("sticky");
-    }
-    if (window.scrollY >= 100) {
-        nav.classList.add("inView");
-    } else if (window.scrollY <= 1) {
-        nav.classList.remove("inView");
+        nav.classList.add("active");
+    } else {
+        nav.classList.remove("active");
     }
 }
 
